a50f7fedfa50e427a5cf6098f6389ed6d5f04ce2
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-07-15 12:33:51 -0700

    laid out basic UI

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -9,7 +9,18 @@ namespace Bakery.Models
   {
     public static void Main()
     {
-      
+      Console.WriteLine("Welcome to the Cafe");
+      Console.WriteLine("Bread: $5.00 a loaf");
+      Console.WriteLine("Bread Special: Buy 2, get 1 free (every 3rd loaf of bread is free.)\n");
+      Console.WriteLine("Pastry: Buy 1 for $2.00 or 3 for $5.00");
+      Console.WriteLine("How many loafs of bread would you like to buy?");
+      int breadResponse = int.Parse(Console.ReadLine());
+      Console.WriteLine("How many Pastries would you like to buy?");
+      int pastryResponse = int.Parse(Console.ReadLine());
+      Console.WriteLine("For " + userCoffee.Quantity + " loafs of bread it will be $" + userCoffee.CoffeeCost());
+      Console.WriteLine("For " + userCoffee.Quantity + " Pastries it will be $" + userCoffee.CoffeeCost());
+      Console.WriteLine("Total Cost: $" + userCoffee.CoffeeCost());
+      Console.WriteLine("Do you want to purchase more?");
     }
   }
 }
