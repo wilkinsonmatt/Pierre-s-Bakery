158b1047c07c2e2f47384e2d4cf344539eeafbf2
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-07-17 20:07:41 -0700

    finished UI

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -9,18 +9,51 @@ namespace Bakery.Models
   {
     public static void Main()
     {
-      Console.WriteLine("\n\n~~~~~~ Welcome to Pierre's Bakery ~~~~~~\n\n\n");
-      Console.WriteLine("Bread: $5.00 a loaf (Buy 2, get 1 free)\n\n");
-      Console.WriteLine("Pastry: Buy 1 for $2.00 or 3 for $5.00\n\n\n");
+      Console.WriteLine("\n\n~~~~~~ Welcome to Pierre's Bakery ~~~~~~");
+      Shop();
+    }
+
+    public static void Shop()
+    {
+      Console.WriteLine("\n\n\n***Bread: $5.00 a loaf (Buy 2, get 1 free)***\n\n");
+      Console.WriteLine("***Pastry: Buy 1 for $2.00 or 3 for $5.00***\n\n\n");
+
       Console.WriteLine("How many loafs of bread would you like to buy?");
-      int breadOder = int.Parse(Console.ReadLine());
-      Bread userBreadOrder = new Bread(breadOder); 
-      // Console.WriteLine("How many Pastries would you like to buy?");
-      // int pastryResponse = int.Parse(Console.ReadLine());
-      // Console.WriteLine("For " + userCoffee.Quantity + " loafs of bread it will be $" + userCoffee.CoffeeCost());
-      // Console.WriteLine("For " + userCoffee.Quantity + " Pastries it will be $" + userCoffee.CoffeeCost());
-      // Console.WriteLine("Total Cost: $" + userCoffee.CoffeeCost());
-      // Console.WriteLine("Do you want to purchase more?");
+      int breadQuantity = int.Parse(Console.ReadLine());
+      Bread userBreadOrder = new Bread(breadQuantity); 
+
+      Console.WriteLine("How many Pastries would you like to buy?");
+      int pastryQuantity = int.Parse(Console.ReadLine());
+      Pastry userPastryOrder = new Pastry(pastryQuantity); 
+
+      Console.WriteLine("\n\nFor " + breadQuantity + " loafs of bread it will be $" + userBreadOrder.GetPriceOfBreadOrder());
+      Console.WriteLine("For " + pastryQuantity + " Pastries it will be $" + userPastryOrder.GetPriceOfPastryOrder());
+      Console.WriteLine("Total Cost: $" + (userPastryOrder.GetPriceOfPastryOrder() + userBreadOrder.GetPriceOfBreadOrder()));
+
+      while(true)
+      {
+        Console.WriteLine("\n\nDo you want to shop again?");
+        Console.WriteLine("1:Yes  2:No");
+        int response = int.Parse(Console.ReadLine());
+
+        switch(response)
+        {
+          case 1:
+            Shop();
+            break;
+          case 2:
+            End();
+            break;
+          default:
+            Console.WriteLine("Error: that wasn't a 1 or 2. Try again");
+            break;
+        }
+      }
+    }
+
+    public static void End()
+    {
+      Console.WriteLine("Have a great day. Goodbye!");
     }
   }
 }
